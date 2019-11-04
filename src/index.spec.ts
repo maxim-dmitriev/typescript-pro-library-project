cec9973cd1fda06738dfa62fd75a48f30ab0e760
Maxim Dmitriev <dmitrievme@yandex.ru>
2019-11-04 14:48:02 +0300

    Fine tuning lib: added type guards and describe block

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,21 @@
 import { isBoolean } from './index';
 
-test('isBoolean should return true if given value is instance of Boolean', () => {
-  expect(isBoolean(new Boolean())).toEqual(true);
-});
+describe('isBoolean()', () => {
+  it('should return true if given value is instance of Boolean', () => {
+    expect(isBoolean(new Boolean())).toEqual(true);
+  });
 
-test('isBoolean should return true if given value is literal true', () => {
-  expect(isBoolean(true)).toEqual(true);
-});
+  it('should return true if given value is literal true', () => {
+    expect(isBoolean(true)).toEqual(true);
+  });
 
-test('isBoolean should return true if given value is literal false', () => {
-  expect(isBoolean(false)).toEqual(true);
-});
+  it('should return true if given value is literal false', () => {
+    expect(isBoolean(false)).toEqual(true);
+  });
 
-test('isBoolean should return false if given value is falsy value', () => {
-  expect(isBoolean('')).toEqual(false);
-  expect(isBoolean(undefined)).toEqual(false);
-  expect(isBoolean(null)).toEqual(false);
+  it('should return false if given value is falsy value', () => {
+    expect(isBoolean('')).toEqual(false);
+    expect(isBoolean(undefined)).toEqual(false);
+    expect(isBoolean(null)).toEqual(false);
+  });
 });
